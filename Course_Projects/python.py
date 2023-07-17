bbc7db4f3c48c1b5a1218a5c4438de00ba782613
Paulina Kusztelak <117985065+paulina-biocoder@users.noreply.github.com>
2023-07-17 19:31:50 +0200

    Delete python.py

--- Course_Projects/python.py
+++
@@ -1,7 +0,0 @@
-def countdown(n):
-    if n <= 0:
-        print('Odpalenie!')
-    else:
-        print(n)
-        countdown(n-1)
-countdown(4) #TAK SIĘ WYWOŁUJE FUNKCJĘ!! PAMIĘTAJ O TYM!!!!!!!! NAZWA FUNKCJI I W NAWIASIE ARGUMENT PARAMATRU (JAKAŚ WARTOŚĆ). W TYM PRZYPADKU N = 4
